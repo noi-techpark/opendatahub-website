fa9b7382cae62dc852a0e3b4678e4ee399c7df79
Gabriele <gabrieledecandido@flashbeing.com>
2021-03-25 17:51:18 +0100

    setup main navbar

--- src/themes/odh-fbe/static/js/main.js
+++ src/themes/odh-fbe/static/js/main.js
@@ -221,26 +221,26 @@
 
 	// Page Nav
 	var clickMenu = function() {
-		var topVal = ( $(window).width() < 769 ) ? 0 : 58;
+		// var topVal = ( $(window).width() < 769 ) ? 0 : 58;
 
-		$(window).resize(function(){
-			topVal = ( $(window).width() < 769 ) ? 0 : 58;		
-		});
-		$('.fh5co-main-nav a:not([class="external"]), #fh5co-offcanvas a:not([class="external"]), a.fh5co-content-nav:not([class="external"])').click(function(event){
-			var section = $(this).data('nav-section');
+		// $(window).resize(function(){
+		// 	topVal = ( $(window).width() < 769 ) ? 0 : 58;		
+		// });
+		// $('.fh5co-main-nav a:not([class="external"]), #fh5co-offcanvas a:not([class="external"]), a.fh5co-content-nav:not([class="external"])').click(function(event){
+		// 	var section = $(this).data('nav-section');
 
-				if ( $('div[data-section="' + section + '"]').length ) {
+		// 		if ( $('div[data-section="' + section + '"]').length ) {
 
-					$('html, body').animate({
-			        	scrollTop: $('div[data-section="' + section + '"]').offset().top - topVal
-			    	}, 500, 'easeInOutExpo');	
+		// 			$('html, body').animate({
+		// 	        	scrollTop: $('div[data-section="' + section + '"]').offset().top - topVal
+		// 	    	}, 500, 'easeInOutExpo');	
 			    	
-			   }
+		// 	   }
 
-		    event.preventDefault();
+		//     event.preventDefault();
 
 		    // return false;
-		});
+		// });
 
 
 	};
