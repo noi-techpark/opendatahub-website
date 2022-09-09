47719c0bfe4ed959dc4f9af079e1d1c7e5428964
Delia Mennitti <mennittid@gmail.com>
2022-09-09 09:05:39 +0200

    added img to kultivas

---
+++ src/public/js/table-comparison.js
@@ -0,0 +1,31 @@
+// Responsive COmparison Table JS
+
+$("#table-comparison").on("click", "li", function () {
+  var pos = $(this).index() + 2;
+  $("tr").find("td:not(:eq(0))").hide();
+  $("td:nth-child(" + pos + ")").css("display", "table-cell");
+  $("tr").find("th:not(:eq(0))").hide();
+  $("li").removeClass("active");
+  $(this).addClass("active");
+});
+
+// Initialize the media query
+var mediaQuery = window.matchMedia("(min-width: 640px)");
+
+// Add a listen event
+mediaQuery.addListener(doSomething);
+
+// Function to do something with the media query
+function doSomething(mediaQuery) {
+  if (mediaQuery.matches) {
+    $("#table-comparison .sep").attr("colspan", 5);
+  } else {
+    $("#table-comparison .sep").attr("colspan", 2);
+  }
+  if ($("#table-comparison li")[1]) {
+    $("#table-comparison li")[1].click();
+  }
+}
+
+// On load
+doSomething(mediaQuery);
