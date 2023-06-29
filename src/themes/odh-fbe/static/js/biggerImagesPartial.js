eae7a6887a44e09767b2bcf1fd1a8045bb5e7dd0
masino <massimo.maglio.06@gmail.com>
2023-06-29 13:01:13 +0200

    fixed the bugs for the mobile version

---
+++ src/themes/odh-fbe/static/js/biggerImagesPartial.js
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+function closeModal() {
+  let modalElements = document.getElementsByClassName("modalClose");
+
+  if (modalElements.length > 0) {
+    let bootstrapModal = bootstrap.Modal.getInstance(modalElements[0]);
+
+    if (bootstrapModal) {
+      bootstrapModal.hide();
+    }
+  }
+}
+
+function deActiveBiggerImages(){
+        let windowWidth = window.innerWidth;
+        let btns = document.querySelectorAll("#btn-img-open");       
+
+        if (windowWidth < 992) {
+            console.log("Window width is less than 992px");
+            btns.forEach((e) => {
+                e.setAttribute("data-bs-toggle", "");
+                e.style.cursor = "default";
+            })
+            closeModal();
+        }else{
+            console.log("Window width is equal or greater than 992px");
+            btns.forEach((e) => {
+                e.setAttribute("data-bs-toggle", "modal");
+                e.style.cursor = "pointer";
+            })
+        }
+}  
+
+window.addEventListener('resize', deActiveBiggerImages)
+
+document.addEventListener("DOMContentLoaded", deActiveBiggerImages);
+
+window.addEventListener("load", deActiveBiggerImages);
