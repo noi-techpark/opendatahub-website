6a74dbb140de284a3a66cfc94b6fa57605073694
henri <henri@moosbauer.com>
2023-07-11 08:49:14 +0200

    add runtime sorting for events

---
+++ src/themes/odh-fbe/static/js/events-sort.js
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+const upcomingEventsContainer = document.getElementById("upcoming-events");
+const upcomingEvents = upcomingEventsContainer.querySelectorAll(".event-box");
+
+const pastEventsContainer = document.getElementById("past-events");
+
+const currentDate = new Date("May 20, 2025");
+
+Array.from(upcomingEvents)
+  .filter((event) => {
+    const dateEl = event.querySelector(".event-date");
+    const dateParts = dateEl.textContent.split(", ");
+    const date = new Date([dateParts[1], dateParts[2]].join(", "));
+
+    return date < currentDate;
+  })
+  .forEach((event) => {
+    upcomingEventsContainer.removeChild(event);
+    const bgDiv = event.querySelector(".event-card");
+    bgDiv.classList.remove("bg-white");
+    bgDiv.classList.add("bg-darker");
+    pastEventsContainer.prepend(event);
+  });
